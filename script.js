15eef20d6163d086b984031b54ccfb0ed12f6694
Hugo Souza <hugo.html.com@gmail.com>
2021-04-06 16:56:25 -0300

    Adicionando eventos ao modal

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+let modalQt = 1;
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
 //Função para agilizar nas seleções de elementos
 
-
+//Listagem das pizza
 pizzaJson.map((item, index) =>{
    let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
@@ -17,19 +19,29 @@ pizzaJson.map((item, index) =>{
    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1; //Serve para resetar o modal depois que fechar
 
         c('.pizzaBig img').src  = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--sector').innerHTML = pizzaJson[key].sizes;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        //Resetando e verificando se o tamanho grande está selecionado
+        c('.pizzaInfo--size.selected').classList.remove('selected');
         //Pegando tamanhos para colocar no modal
         cs('.pizzaInfo--size').forEach((size, sizeIndex) =>{
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML =  pizzaJson[key].sizes[sizeIndex];
         });
+
+        //Qt padrão que contém no modal
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
         
       
-     
+        //Aqui é o comando para abrir o modal
         c('.pizzaWindowArea').style.display = 'flex';
         c('.pizzaWindowArea').style.opacity = 0;
         setTimeout(()=>{
@@ -42,6 +54,27 @@ pizzaJson.map((item, index) =>{
 
 
 });
+
+//Eventos do modal
+
+function closseModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    },200);
+}
+
+//Selecionando os botões de fechar
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+        item.addEventListener('click', closseModal);
+});
+
+
+
+
+
+
 
 //Mapear todos os itens preencher e mandar para tela
 
